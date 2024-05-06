fbf6fa15304f189256ac03fb0bd40189ca08e31b
DarthEvgenius <clay14@yandex.ru>
2024-05-06 14:48:48 +0300

    data tabs error fix

--- src/js/components/sign-in-tabs.js
+++ src/js/components/sign-in-tabs.js
@@ -1,2 +1,9 @@
 import GraphTabs from 'graph-tabs';
-const tabs = new GraphTabs('sign-in-tabs');
+
+// const tabs = new GraphTabs('sign-in-tabs');
+
+const checkTab = document.querySelector('[data-tabs]')
+
+if (checkTab) {
+  const tabs = new GraphTabs(checkTab.dataset.tabs);
+}
