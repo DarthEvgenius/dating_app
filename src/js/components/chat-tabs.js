30251c0fb8543fdbf8419f8a6966b7586b694724
DarthEvgenius <clay14@yandex.ru>
2024-05-07 01:13:51 +0300

    messages and matches done

--- src/js/components/chat-tabs.js
+++ src/js/components/chat-tabs.js
@@ -4,8 +4,7 @@ import GraphTabs from 'graph-tabs';
 const checkTab = document.querySelector('[data-tabs]').dataset.tabs;
 
 
-if (checkTab === 'sign-in-tabs') {
-  const tabSignIn = new GraphTabs(checkTab);
-  console.log(tabSignIn);
-
+if (checkTab === 'chat-tabs') {
+  const tabsChat = new GraphTabs(checkTab);
+  console.log(tabsChat);
 }
---
+++ src/js/components/tabs.js
@@ -0,0 +1,16 @@
+import GraphTabs from 'graph-tabs';
+
+let tabs = null;
+
+const checkTab = document.querySelector('[data-tabs]')?.dataset.tabs;
+
+
+if (checkTab === 'sign-in-tabs') {
+  tabs = new GraphTabs(checkTab);
+  console.log(tabs);
+
+}
+if (checkTab === 'chat-tabs') {
+  const tabs = new GraphTabs(checkTab);
+  console.log(tabs);
+}
