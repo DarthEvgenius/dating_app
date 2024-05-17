85a254a73e1df96be09563af02e20b4d579ff10f
DarthEvgenius <clay14@yandex.ru>
2024-05-17 14:32:06 +0300

    messages tabs html update. Tabs buttons styles update. Profile tabs height calculation fix

--- app/js/main.js
+++ app/js/main.js
@@ -869,13 +869,14 @@ function setProfileTabsHeight() {
   const profileTabsContainers = arguments[0]?.profileTabsContainers || document.querySelectorAll('.profile-tabs__container');
   const profilePlanBanner = arguments[0]?.profilePlanBanner || document.querySelector('.profile__plan');
   const windowHeight = window.innerHeight;
+  const tabsButtonsHeight = document.querySelector('.tabs__nav').offsetHeight;
   const headerHeight = parseInt(getComputedStyle(profileHeader).getPropertyValue('margin-bottom')) + profileHeader.offsetHeight;
   let height = 0;
   if (profilePlanBanner) {
     const bannerHeight = parseInt(getComputedStyle(profilePlanBanner).getPropertyValue('margin-bottom')) + profilePlanBanner.offsetHeight;
-    height = windowHeight - headerHeight - bannerHeight;
+    height = windowHeight - headerHeight - bannerHeight - tabsButtonsHeight;
   } else {
-    height = windowHeight - headerHeight;
+    height = windowHeight - headerHeight - tabsButtonsHeight;
   }
   profileTabsContainers.forEach(elem => {
     elem.style.maxHeight = `${height}px`;
--- src/js/components/profile-section/profile-sections-height.js
+++ src/js/components/profile-section/profile-sections-height.js
@@ -4,6 +4,7 @@ export function setProfileTabsHeight() {
   const profileTabsContainers = arguments[0]?.profileTabsContainers || document.querySelectorAll('.profile-tabs__container')
   const profilePlanBanner = arguments[0]?.profilePlanBanner || document.querySelector('.profile__plan')
   const windowHeight = window.innerHeight
+  const tabsButtonsHeight = document.querySelector('.tabs__nav').offsetHeight
 
   const headerHeight = parseInt(getComputedStyle(profileHeader).getPropertyValue('margin-bottom')) + profileHeader.offsetHeight
 
@@ -12,9 +13,9 @@ export function setProfileTabsHeight() {
   if (profilePlanBanner) {
     const bannerHeight = parseInt(getComputedStyle(profilePlanBanner).getPropertyValue('margin-bottom')) + profilePlanBanner.offsetHeight
 
-    height = windowHeight - headerHeight - bannerHeight
+    height = windowHeight - headerHeight - bannerHeight - tabsButtonsHeight
   } else {
-    height = windowHeight - headerHeight
+    height = windowHeight - headerHeight - tabsButtonsHeight
   }
 
   profileTabsContainers.forEach(elem => {
