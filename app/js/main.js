a2bd17c8836182bdcb5715e8aa8b5a36f8f0651c
DarthEvgenius <clay14@yandex.ru>
2024-05-19 16:41:53 +0300

    try catch on fetch chat

--- app/js/main.js
+++ app/js/main.js
@@ -791,23 +791,27 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _getCookie_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../getCookie.js */ "./src/js/components/getCookie.js");
 
 async function fetchChatInfo(chatId) {
-  const token = '"dGVzdGVyNTU=.cGJrZGYyX3NoYTI1NiQ3MjAwMDAkZjc3ZTY0ZTA0OWI5Y2ZiYjBlNTk1ZmViMzNkNTJlZmM1YTIxMWYzNGUxYzUyMWMxZDEzYzg4ODU5MTQyZjJmOSRZMDI5K25NbVd2bFc3YzYwYTE2U2ZUQXd1V1J5NjFNb3JGUnRaMlVIVUZJPQ=="';
-  // const token = getCookie("ws_login")
-
+  // const token = '"dGVzdGVyNTU=.cGJrZGYyX3NoYTI1NiQ3MjAwMDAkZjc3ZTY0ZTA0OWI5Y2ZiYjBlNTk1ZmViMzNkNTJlZmM1YTIxMWYzNGUxYzUyMWMxZDEzYzg4ODU5MTQyZjJmOSRZMDI5K25NbVd2bFc3YzYwYTE2U2ZUQXd1V1J5NjFNb3JGUnRaMlVIVUZJPQ=="'
+  const token = (0,_getCookie_js__WEBPACK_IMPORTED_MODULE_0__.getCookie)("ws_login");
   let url = `http://vm592483.eurodir.ru/api/v1/chat/${chatId}/`;
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `${token}`
-    }
-  });
-  if (!response.ok) {
-    const message = `An error has occured: ${response.status}`;
-    throw new Error(message);
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `${token}`
+      }
+    });
+    localStorage.setItem('chatInfo', response);
+    const chatInfo = await response.json();
+    return chatInfo;
+  } catch (error) {
+    console.log(error);
   }
-  localStorage.setItem('chatInfo', response);
-  const chatInfo = await response.json();
-  return chatInfo;
+
+  // if (!response.ok) {
+  //   const message = `An error has occured: ${response.status}`;
+  //   throw new Error(message);
+  // }
 }
 
 /***/ }),
--- src/js/components/chat/fetchChatInfo.js
+++ src/js/components/chat/fetchChatInfo.js
@@ -1,26 +1,34 @@
 import { getCookie } from '../getCookie.js'
 
 export async function fetchChatInfo(chatId) {
-  const token = '"dGVzdGVyNTU=.cGJrZGYyX3NoYTI1NiQ3MjAwMDAkZjc3ZTY0ZTA0OWI5Y2ZiYjBlNTk1ZmViMzNkNTJlZmM1YTIxMWYzNGUxYzUyMWMxZDEzYzg4ODU5MTQyZjJmOSRZMDI5K25NbVd2bFc3YzYwYTE2U2ZUQXd1V1J5NjFNb3JGUnRaMlVIVUZJPQ=="'
-  // const token = getCookie("ws_login")
+  // const token = '"dGVzdGVyNTU=.cGJrZGYyX3NoYTI1NiQ3MjAwMDAkZjc3ZTY0ZTA0OWI5Y2ZiYjBlNTk1ZmViMzNkNTJlZmM1YTIxMWYzNGUxYzUyMWMxZDEzYzg4ODU5MTQyZjJmOSRZMDI5K25NbVd2bFc3YzYwYTE2U2ZUQXd1V1J5NjFNb3JGUnRaMlVIVUZJPQ=="'
+  const token = getCookie("ws_login")
 
   let url = `http://vm592483.eurodir.ru/api/v1/chat/${chatId}/`
 
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `${token}`
-    }
-  })
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `${token}`
+      }
+    })
 
-  if (!response.ok) {
-    const message = `An error has occured: ${response.status}`;
-    throw new Error(message);
+    localStorage.setItem('chatInfo', response)
+
+    const chatInfo = await response.json()
+
+    return chatInfo
+
+  } catch(error) {
+    console.log(error);
+
   }
 
-  localStorage.setItem('chatInfo', response)
+  // if (!response.ok) {
+  //   const message = `An error has occured: ${response.status}`;
+  //   throw new Error(message);
+  // }
 
-  const chatInfo = await response.json()
 
-  return chatInfo
 }
